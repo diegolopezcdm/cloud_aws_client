54f05f8e71f4759891d1d9dff0dc381963d42227
diegol <diegolopezcdm@gmail.com>
2020-09-12 03:14:24 -0500

    first commit

---
+++ src/app/_shared/constants.ts
@@ -0,0 +1,11 @@
+export const HOST_BACKEND = `http://loadbalancer-diego-96899420.us-east-2.elb.amazonaws.com`
+//export const HOST_BACKEND = `http://192.168.99.100`
+//export const HOST_BACKEND = `https://a5q8gn7rkb.execute-api.us-east-1.amazonaws.com/prod`
+
+export const TIME_UPDATE_GEOLOCALIZATION = 60000;
+export const RADIO = 0.029;
+export const ZOOM = 16;
+export const TOKEN_NAME = "idToken";
+export const REFRESH_TOKEN_NAME = "refreshToken";
+export const ACCESS_TOKEN_NAME = "accessToken";
+export const PARAM_USUARIO = "usuario";
